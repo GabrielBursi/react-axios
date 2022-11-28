844f943b2ead7402410c0fd94bbadd30d0c01477
Gabriel Bursi <gabriellbursii@gmail.com>
2022-11-28 20:31:02 -0300

    criando função da api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,35 @@
+import React, { useState,useEffect } from 'react';
+
+import axios from 'axios'
+import { Link } from 'react-router-dom';
+
 function HomePage() {
+
+    const [posts, setPosts] = useState([]);
+
+    async function getPosts(){
+        try {
+            const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+            const data = res.data
+            setPosts(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getPosts();
+    }, []);
+
     return (
         <>
-            Home
+            {posts.length === 0 ? (<p>Carregando...</p>) : (posts.map(post => (
+                <div className='post' key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                    <Link to={`/posts/${post.id}`} className='btn'>Read more</Link>
+                </div>
+            )))}
         </>
     );
 }
