fda9e548b82a5981659fe9019049f9f8dabdc5d3
Gabriel Bursi <gabriellbursii@gmail.com>
2022-11-29 15:49:19 -0300

    criando newPost e estilo

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -1,8 +1,21 @@
+import './styles/newPost.css'
+
 function NewPostPage() {
     return (
-        <>
-            New post
-        </>
+        <div className="new-post">
+            <h2>Create a new Post:</h2>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="title">Title:</label>
+                    <input type='text' name="title" id="title" placeholder="Write here"/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="body">Content:</label>
+                    <textarea name="body" id="body" placeholder="Write the content"></textarea>
+                </div>
+                <button>Create Post</button>
+            </form>
+        </div>
     );
 }
 
